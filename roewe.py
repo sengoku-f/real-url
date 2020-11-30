cbe6fba67728a1429051a31da9784167c29d65ec
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2020-11-30 22:05:20 +0800

    重写脚本,改为函数

--- roewe.py
+++ roewe.py
@@ -1,19 +1,25 @@
 # 获取荣威直播的真实流媒体地址，默认最高画质。
+# 直播链接
+# https://www.roewe.com.cn/about-us/new&activity/html/activity/live/RSHBund/share/index.html?channel=wechat&macAddress=159680031632671805&userId=10000003755407&shareTime=1597287406050&from=singlemessage-
 
-import sys
 import requests
 import re
 
+def get_real_url(url):
+  # 获取传入参数
+  roewe_url = re.search(r'([a-zA-z]+:\/\/[^\s]*roewe\.com[^\s]*\/)index\.html[^\s]*', url).group(1) + 'js/parameter.js'
 
-# 获取传入参数
-roewe_url = re.search(r'([a-zA-z]+:\/\/[^\s]*roewe\.com[^\s]*\/)index\.html[^\s]*', sys.argv[1]).group(1) + 'js/parameter.js'
+  r = requests.get(roewe_url)
+  live_id = re.search(r'var\ssaicLiveId\s=\s\'(\d+)\';', (r.text)).group(1)
 
-r = requests.get(roewe_url)
-live_id = re.search(r'var\ssaicLiveId\s=\s\'(\d+)\';', (r.text)).group(1)
+  live_url = "https://live.vhall.com/webinar/inituser/{}?embed=video".format(live_id)
 
-live_url = "https://live.vhall.com/webinar/inituser/{}?embed=video".format(live_id)
+  s = re.search(r'\/\/[^\s]*record\.m3u8', requests.get(live_url).text).group()
 
-s = re.search(r'\/\/[^\s]*record\.m3u8', requests.get(live_url).text).group()
+  # 返回真实链接
+  return 'http:' + s
 
-# 打印真实链接
-print('http:' + str(s))
+if __name__ == '__main__':
+    url = input('输入荣威直播链接：\n')
+    print(get_real_url(url))
+
--- yiche.py
+++ yiche.py
@@ -1,18 +1,20 @@
 # 获取易车直播的真实流媒体地址，默认最高画质。
 # http://live.yiche.com/live/58911.html
-import sys
+
 import requests
 import re
 
+def get_real_url(url):
+  # 获取传入参数
+  live_id = re.search(r'[a-zA-z]+:\/\/[^\s]*yiche\.com[^\s]*\/(\d+)\.html[^\s]*', url).group(1)
 
-# 获取传入参数
-live_id = re.search(r'[a-zA-z]+:\/\/[^\s]*yiche\.com[^\s]*\/(\d+)\.html[^\s]*', sys.argv[1]).group(1)
+  live_url = "http://live.yiche.com/web/pc/live/ShowIframe?liveid={}&viewname=IframeNormShow&support=1&dm=1".format(live_id)
 
-live_url = "http://live.yiche.com/web/pc/live/ShowIframe?liveid={}&viewname=IframeNormShow&support=1&dm=1".format(live_id)
+  s = re.search(r'\/\/[^\s]*\.(m3u8|mp4)', requests.get(live_url).text).group()
 
-# r = requests.get(live_url)
+  # 打印真实链接
+  return 'http:' + s
 
-s = re.search(r'\/\/[^\s]*\.(m3u8|mp4)', requests.get(live_url).text).group()
-
-# 打印真实链接
-print('http:' + str(s))
+if __name__ == '__main__':
+    url = input('输入易车直播链接：\n')
+    print(get_real_url(url))
